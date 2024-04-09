96dad23c3325d69e08606d196d45fed395ee282d
Simon <35050400+superchelo@users.noreply.github.com>
2024-04-09 18:45:53 -0400

    removed payment reminder
    removed payment reminder

--- coach.js
+++ coach.js
@@ -95,16 +95,81 @@ function revealCalendar() {
 }
 
 
+function addPractice() {
+    var practiceDate = document.getElementById("cal-prac-picker").value;
+    if (practiceDate == "") {
+        alert("Please choose a date");
+        return;
+    }
+
+    var repeatedDate = false;
+    var currPracDates = Array.from(document.querySelectorAll('#practices-to-add>li'), (li) => {
+        console.log(li.textContent);
+        if (li.textContent.includes(practiceDate)) {
+            alert("Repeated date attempted to add")
+            repeatedDate = true;
+            return;
+        }
+    });
+
+    if (!repeatedDate) {
+        var practiceList = document.getElementById("practices-to-add");
+        var liElement = document.createElement('li');
+        var liBtn = document.createElement('btn');
+    
+        liElement.textContent = practiceDate + " ";
+        liBtn.id = "prac-rem-btn";
+        liBtn.textContent = '×';
+        liBtn.style.color = "white";
+        liBtn.style.background = "red";
+        liBtn.style.borderRadius = "5px";
+
+        liBtn.onclick = removePractice;
+        // liBtn.setAttribute('onclick', )
+
+        liElement.appendChild(liBtn);
+        practiceList.appendChild(liElement);
+    }
+    document.getElementById("cal-prac-picker").value = null;
+    return;
+}
+
+function removePractice() {
+    this.getElementById
+    var parentElement = document.getElementById("myLI").parentElement.nodeName
+    console.log("removed");
+}
+
+
+function addAllPractices() {
+
+
+    // var listofPracticeDates = document.getElementById("practices-to-add");
+
+    var datesToAdd = Array.from(document.querySelectorAll('#practices-to-add>li'), (li) => {
+        console.log(li.textContent)
+    });
+
+    // console.log(listofPracticeDates);
+    document.getElementById("cal-prac-picker").value = null;
+    return;
+}
+
 function storeInput() {
     // Step 1: Get a reference to the input element
     var usernameElement = document.getElementById('usernamesInput');
     var messageElement = document.getElementById('messageInput');
     // Step 2: Retrieve the value entered by the user
     var usernameInput = usernameElement.value;
+    var usernames = usernameInput.split(",");
+
     var messageInput = messageElement.value;
     // Step 3: Store the value as needed
     // For example, you can store it in a variable
     //var storedValue = usernameInput;
-    console.log("Stored value:", usernameInput);
+    for (var i = 0; i < usernames.length; i++){
+        send(usernames[i], messageInput)
+    }
+    console.log("Stored value:", usernames);
     console.log("Stored value:", messageInput);
 }
